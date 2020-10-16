282ab1bff93fd4636edb48af223ec8526a04c637
chusang23 <45816771+chusang23@users.noreply.github.com>
2020-10-16 12:37:41 +0900

    realtime nweet

--- src/components/App.js
+++ src/components/App.js
@@ -1,15 +1,16 @@
 import React, { useEffect, useState } from 'react';
 import AppRouter from "components/Router";
 import { authService } from "fbase";
-import { auth } from 'firebase';
 
 function App() {
   const[init, setInit] = useState(false);
   const [isLoggedIn, setIsLoggedIn] = useState(authService.currentUser);
+  const [userObj, setUserObj] = useState(null);
   useEffect(() => {
     authService.onAuthStateChanged((user) => {
       if(user){
         setIsLoggedIn(true);
+        setUserObj(user);
       } else {
         setIsLoggedIn(false);
       }
@@ -18,7 +19,7 @@ function App() {
   }, []);
   return (
   <>
-    {init ? <AppRouter isLoggedIn={isLoggedIn} /> : "Initializing..."}
+    {init ? <AppRouter isLoggedIn={isLoggedIn} userObj={userObj} /> : "Initializing..."}
     <footer>&copy; {new Date().getFullYear()} Nwitter </footer>
   </>
   );
