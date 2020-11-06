d940d1477256bdef7237625f61eebd19ababcd2f
chusang23 <45816771+chusang23@users.noreply.github.com>
2020-11-06 23:50:57 +0900

    styles

--- src/components/Navigation.js
+++ src/components/Navigation.js
@@ -1,17 +1,18 @@
-import React from "react"
-import {Link} from "react-router-dom"
+import React from "react";
+import { Link } from "react-router-dom";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faTwitter } from "@fortawesome/free-brands-svg-icons";
 import { faUser } from "@fortawesome/free-solid-svg-icons";
 
-const Navigation = ({ userObj }) => <nav>
+const Navigation = ({ userObj }) => (
+  <nav>
     <ul style={{ display: "flex", justifyContent: "center", marginTop: 50 }}>
-        <li>
+      <li>
         <Link to="/" style={{ marginRight: 10 }}>
           <FontAwesomeIcon icon={faTwitter} color={"#04AAFF"} size="2x" />
         </Link>
-        </li>
-        <li>
+      </li>
+      <li>
         <Link
           to="/profile"
           style={{
@@ -29,7 +30,8 @@ const Navigation = ({ userObj }) => <nav>
               : "Profile"}
           </span>
         </Link>
-        </li>
+      </li>
     </ul>
-</nav>;
+  </nav>
+);
 export default Navigation;
